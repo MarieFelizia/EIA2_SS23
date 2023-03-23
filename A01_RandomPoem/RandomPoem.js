4fb3a35975e46eefd30766c9cdf8ef0b862db191
MarieFelizia <115719520+MarieFelizia@users.noreply.github.com>
2023-03-23 13:00:25 +0100

    Arrays

--- A01_RandomPoem/RandomPoem.js
+++ A01_RandomPoem/RandomPoem.js
@@ -1,4 +1,12 @@
 "use strict";
-let random = ["Hello"];
-console.log(random);
+var RandomPoem;
+(function (RandomPoem) {
+    let subjects = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
+    console.log(subjects);
+    let predicates = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
+    console.log(predicates);
+    let objects = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
+    console.log(objects);
+})(RandomPoem || (RandomPoem = {}));
+;
 //# sourceMappingURL=RandomPoem.js.map
--- A01_RandomPoem/RandomPoem.ts
+++ A01_RandomPoem/RandomPoem.ts
@@ -1,2 +1,15 @@
-let random: string[] = ["Hello"]; 
-console.log(random); 
+namespace RandomPoem{ 
+
+let subjects: string[] = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore" ]; 
+console.log(subjects);
+
+let predicates: string[] = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
+console.log(predicates);
+
+let objects: string[] = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
+console.log(objects);
+
+
+
+
+}; 
