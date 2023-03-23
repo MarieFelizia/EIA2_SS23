a283ab9f9c9b93bd05e78d69559af29b90fbb0df
MarieFelizia <115719520+MarieFelizia@users.noreply.github.com>
2023-03-23 15:35:10 +0100

    TS Part 1

--- A01_RandomPoem/RandomPoem.js
+++ A01_RandomPoem/RandomPoem.js
@@ -7,6 +7,19 @@ var RandomPoem;
     console.log(predicates);
     let objects = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
     console.log(objects);
+    let array1 = [];
+    let array2 = [];
+    let array3 = [];
+    for (let i = subjects.length; i >= 1; i--) {
+        console.log(i);
+        getVerse(array1[1], array2[2], array3[3]);
+    }
+    ;
+    function getVerse(_sub, _pre, _obj) {
+        console.log(_sub + " " + _pre + " " + _obj);
+        return _sub + " " + _pre + " " + _obj;
+    }
+    ;
 })(RandomPoem || (RandomPoem = {}));
 ;
 //# sourceMappingURL=RandomPoem.js.map
--- A01_RandomPoem/RandomPoem.ts
+++ A01_RandomPoem/RandomPoem.ts
@@ -9,6 +9,24 @@ console.log(predicates);
 let objects: string[] = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
 console.log(objects);
 
+let array1: string[] = [];
+let array2: string[] = [];
+let array3: string[] = []; 
+
+
+for (let i: number = subjects.length ; i >= 1; i--) { 
+    console.log(i);
+    getVerse(array1[1], array2[2], array3[3])
+
+};
+
+
+function getVerse(_sub: string, _pre: string, _obj:string):string { 
+    console.log(_sub +" " + _pre +" " + _obj);
+    return _sub +" " + _pre +" " + _obj; 
+
+}; 
+
 
 
 
