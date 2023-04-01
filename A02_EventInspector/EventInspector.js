daf1fa48074f4181ae1317cecb5b985321240dc1
MarieFelizia <115719520+MarieFelizia@users.noreply.github.com>
2023-04-01 16:20:29 +0200

    change

--- A02_EventInspector/EventInspector.js
+++ A02_EventInspector/EventInspector.js
@@ -1,6 +1,13 @@
 "use strict";
-var EventsInspector;
-(function (EventsInspector) {
+var EventInspector;
+(function (EventInspector) {
+    /*
+    Aufgabe: <A02_EventInspector>
+    Name:<Marie Eckl>
+    Matrikel: <272409>
+    Datum: <01.04.23>
+    Zusammenarbeit mit Pia Schwer, Theresa Hauser
+    */
     window.addEventListener("load", handleLoad);
     function handleLoad() {
         let div0 = document.getElementById("div0");
@@ -14,8 +21,41 @@ var EventsInspector;
         div0.addEventListener("keyup", logInfo);
         div1.addEventListener("click", logInfo);
         div1.addEventListener("keyup", logInfo);
+        let button = document.getElementById("button");
+        button.addEventListener("click", customEvent);
+        document.addEventListener("myCustomEvent", Output);
     }
     ;
-})(EventsInspector || (EventsInspector = {}));
+    function setInfoBox(_event) {
+        let span = document.getElementById("span");
+        let x = _event.clientX;
+        let y = _event.clientY;
+        let xx = x + 10;
+        let yy = y + 10;
+        //console.log(x,y);
+        span.innerHTML = "Position x: " + x + "  Position y: " + y;
+        span.style.position = "absolute";
+        span.style.top = `${yy}px`;
+        span.style.left = `${xx}px`;
+    }
+    ;
+    function logInfo(_event) {
+        console.log(_event.type);
+        console.log(_event.target);
+        console.log(_event.currentTarget);
+        console.log(_event);
+    }
+    ;
+    function customEvent(_event) {
+        let button = document.getElementById("button");
+        let customEventNew = new CustomEvent("myCustomEvent", { bubbles: true });
+        button.dispatchEvent(customEventNew);
+    }
+    ;
+    function Output(_event) {
+        console.log("Mein Custom Event wird gerade ausgegeben.");
+    }
+    ;
+})(EventInspector || (EventInspector = {}));
 ;
 //# sourceMappingURL=EventInspector.js.map
--- A02_EventInspector/EventInspector.ts
+++ A02_EventInspector/EventInspector.ts
@@ -1,32 +1,62 @@
-namespace EventsInspector { 
+namespace EventInspector {
+    /*
+    Aufgabe: <A02_EventInspector>
+    Name:<Marie Eckl>
+    Matrikel: <272409>
+    Datum: <01.04.23>
+    Zusammenarbeit mit Pia Schwer, Theresa Hauser 
+    */
 
-window.addEventListener("load",handleLoad);
+    window.addEventListener("load", handleLoad);
+
+    function handleLoad() {
+        let div0: HTMLDivElement = <HTMLDivElement>document.getElementById("div0");
+        let div1: HTMLDivElement = <HTMLDivElement>document.getElementById("div1")
+
+        document.addEventListener("mousemove", setInfoBox);
+        document.addEventListener("click", logInfo);
+        document.addEventListener("keyup", logInfo);
+        document.body.addEventListener("click", logInfo);
+        document.body.addEventListener("keyup", logInfo);
+        div0.addEventListener("click", logInfo);
+        div0.addEventListener("keyup", logInfo);
+        div1.addEventListener("click", logInfo);
+        div1.addEventListener("keyup", logInfo);
+        let button: HTMLButtonElement = <HTMLButtonElement>document.getElementById("button");
+        button.addEventListener("click", customEvent);
+        document.addEventListener("myCustomEvent", Output);
+    };
+
+    function setInfoBox(_event: MouseEvent) {
+        let span: HTMLSpanElement = <HTMLSpanElement>document.getElementById("span");
+        let x: number = _event.clientX;
+        let y: number = _event.clientY;
+        let xx: number = x + 10;
+        let yy: number = y + 10;
+        //console.log(x,y);
+        span.innerHTML = "Position x: " + x + "  Position y: " + y;
+        span.style.position = "absolute";
+        span.style.top = `${yy}px`;
+        span.style.left = `${xx}px`;
+    };
+
+    function logInfo(_event: Event) {
+        console.log(_event.type);
+        console.log(_event.target);
+        console.log(_event.currentTarget);
+        console.log(_event);
+    };
+
+    function customEvent(_event: MouseEvent) {
+        let button: HTMLButtonElement = <HTMLButtonElement>document.getElementById("button");
+        let customEventNew = new CustomEvent("myCustomEvent", { bubbles: true });
+        button.dispatchEvent(customEventNew);
+    };
+
+    function Output(_event: Event) {
+        console.log("Mein Custom Event wird gerade ausgegeben.");
+    };
+
+};
 
 
-
-function handleLoad(){ 
-    let div0: HTMLDivElement = <HTMLDivElement> document.getElementById("div0");
-    let div1: HTMLDivElement = <HTMLDivElement> document.getElementById("div1")
-
-    document.addEventListener("mousemove",setInfoBox);
-    document.addEventListener("click", logInfo);
-    document.addEventListener("keyup", logInfo);
-    document.body.addEventListener("click", logInfo);
-    document.body.addEventListener("keyup", logInfo);
-    div0.addEventListener("click", logInfo);
-    div0.addEventListener("keyup", logInfo);
-    div1.addEventListener("click", logInfo);
-    div1.addEventListener("keyup", logInfo);
-}; 
-
-
-
-
-
-
-
-
-
-
-
-}; 
