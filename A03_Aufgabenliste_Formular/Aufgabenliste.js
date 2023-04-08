7bf11c6c212b2d38aed868252413aac4442e6c41
MarieFelizia <115719520+MarieFelizia@users.noreply.github.com>
2023-04-08 18:46:37 +0200

    Konsolenausgaben

--- A03_Aufgabenliste_Formular/Aufgabenliste.js
+++ A03_Aufgabenliste_Formular/Aufgabenliste.js
@@ -1,2 +1,35 @@
 "use strict";
+var Aufgabenliste;
+(function (Aufgabenliste) {
+    console.log("App wird gestartet");
+    window.addEventListener("load", handleLoad);
+    function handleLoad() {
+        document.getElementById("radioButton")?.addEventListener("click", radioButton);
+        document.getElementById("chooseName")?.addEventListener("click", chooseName);
+        document.getElementById("deadline")?.addEventListener("click", deadline);
+        document.getElementById("comment")?.addEventListener("click", comment);
+        document.getElementById("deleteTask")?.addEventListener("click", deleteTask);
+    }
+    ;
+    function radioButton() {
+        console.log("Radio Button gedrückt");
+    }
+    ;
+    function chooseName() {
+        console.log("Name ausgewählt");
+    }
+    ;
+    function deadline() {
+        console.log("Uhrzeit und Datum geändert");
+    }
+    ;
+    function comment() {
+        console.log("Aufgabe kommentiert");
+    }
+    ;
+    function deleteTask() {
+        console.log("Aufgabe löschen Button gedrückt");
+    }
+    ;
+})(Aufgabenliste || (Aufgabenliste = {}));
 //# sourceMappingURL=Aufgabenliste.js.map
